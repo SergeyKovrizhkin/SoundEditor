354eb03c368df0e7ddc72b07496f2592f5e125b4
Kovrizhkin Sergey <li-sergej95@yandex.ru>
2021-05-03 14:39:37 +0300

    Get file path

--- app/src/main/java/com/school/soundeditor/main/MainFragment.kt
+++ app/src/main/java/com/school/soundeditor/main/MainFragment.kt
@@ -2,13 +2,22 @@ package com.school.soundeditor.main
 
 import android.Manifest
 import android.app.AlertDialog
+import android.content.ContentUris
+import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.database.Cursor
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.os.Environment
+import android.provider.DocumentsContract
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.DividerItemDecoration
@@ -27,6 +36,7 @@ internal class MainFragment : Fragment(), MainScreenView {
     private var savedScrollingPosition = 0
     private var onSaveDataListener: OnSaveData? = null
     private var onSaveScrollingPositionListener: OnSaveScrollingPosition? = null
+    private var fileSrc: String? = null
 
 
     internal fun setListener(listener: ShowItemForPlayback) {
@@ -146,6 +156,105 @@ internal class MainFragment : Fragment(), MainScreenView {
         chooseFile = Intent.createChooser(chooseFile, "Choose a file")
         //start activity and wait for the result
         startActivityForResult(chooseFile, RC_PICK_FILE)
+    }
+
+    private fun isExternalStorageDocument(uri: Uri): Boolean {
+        return "com.android.externalstorage.documents" == uri.authority
+    }
+
+    private fun isDownloadsDocument(uri: Uri): Boolean {
+        return "com.android.providers.downloads.documents" == uri.authority
+    }
+
+    private fun getDataColumn(
+        context: Context, uri: Uri?, selection: String?,
+        selectionArgs: Array<String>?
+    ): String? {
+        var cursor: Cursor? = null
+        val column = "_data"
+        val projection = arrayOf(
+            column
+        )
+        try {
+            cursor = context.contentResolver.query(
+                uri!!, projection, selection, selectionArgs,
+                null
+            )
+            if (cursor != null && cursor.moveToFirst()) {
+                val columnIndex: Int = cursor.getColumnIndexOrThrow(column)
+                return cursor.getString(columnIndex)
+            }
+        } finally {
+            cursor?.close()
+        }
+        return null
+    }
+
+    private fun isMediaDocument(uri: Uri): Boolean {
+        return "com.android.providers.media.documents" == uri.authority
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    fun getPath(context: Context, uri: Uri): String? {
+        if (DocumentsContract.isDocumentUri(context, uri)) {
+            // ExternalStorageProvider
+            if (isExternalStorageDocument(uri)) {
+                val docId = DocumentsContract.getDocumentId(uri)
+                val split = docId.split(":".toRegex()).toTypedArray()
+                val type = split[0]
+                if ("primary".equals(type, ignoreCase = true)) {
+                    return Environment.getExternalStorageDirectory().toString() + "/" + split[1]
+                }
+            } else if (isDownloadsDocument(uri)) {
+                val id = DocumentsContract.getDocumentId(uri)
+                val contentUri = ContentUris.withAppendedId(
+                    Uri.parse("content://downloads/public_downloads"), java.lang.Long.valueOf(id)
+                )
+                return getDataColumn(context, contentUri, null, null)
+            } else if (isMediaDocument(uri)) {
+                val docId = DocumentsContract.getDocumentId(uri)
+                val split = docId.split(":".toRegex()).toTypedArray()
+                val type = split[0]
+                var contentUri: Uri? = null
+                when (type) {
+                    "image" -> {
+                        contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+                    }
+                    "video" -> {
+                        contentUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI
+                    }
+                    "audio" -> {
+                        contentUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI
+                    }
+                }
+                val selection = "_id=?"
+                val selectionArgs = arrayOf(
+                    split[1]
+                )
+                return getDataColumn(context, contentUri, selection, selectionArgs)
+            }
+        } else if ("content".equals(uri.scheme, ignoreCase = true)) {
+            return getDataColumn(context, uri, null, null)
+        } else if ("file".equals(uri.scheme, ignoreCase = true)) {
+            return uri.path
+        }
+        return null
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == RC_PICK_FILE) {
+            if (data != null) {
+                //get uri from intent returned
+                val uri: Uri? = data.data
+                //get file path of the uri
+                fileSrc = uri?.let { getPath(requireContext(), it) }
+                //update chosen file textView with filesrc
+                Toast.makeText(requireContext(), fileSrc, Toast.LENGTH_LONG).show()
+                //handleFileChosenMediaPlayer(fileSrc)
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data)
     }
 
     private fun getTrackList(): RecyclerSavedListData {
