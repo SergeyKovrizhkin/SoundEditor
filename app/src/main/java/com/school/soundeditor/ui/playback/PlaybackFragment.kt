db75c3e6e8627c919c10833661b86d1d3213b206
Kovrizhkin Sergey <li-sergej95@yandex.ru>
2021-05-18 17:51:17 +0300

    Trim and loop added

--- app/src/main/java/com/school/soundeditor/ui/playback/PlaybackFragment.kt
+++ app/src/main/java/com/school/soundeditor/ui/playback/PlaybackFragment.kt
@@ -1,23 +1,43 @@
 package com.school.soundeditor.ui.playback
 
+
+import android.Manifest
+import android.app.Activity
+import android.content.pm.PackageManager
+import android.media.MediaMetadataRetriever
 import android.media.MediaPlayer
 import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
 import android.os.Handler
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.SeekBar
+import android.widget.SeekBar.OnSeekBarChangeListener
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
+import com.coremedia.iso.boxes.Container
+import com.googlecode.mp4parser.authoring.Movie
+import com.googlecode.mp4parser.authoring.Track
+import com.googlecode.mp4parser.authoring.builder.DefaultMp4Builder
+import com.googlecode.mp4parser.authoring.container.mp4.MovieCreator
+import com.googlecode.mp4parser.authoring.tracks.AppendTrack
+import com.googlecode.mp4parser.authoring.tracks.CroppedTrack
 import com.school.soundeditor.R
 import com.school.soundeditor.ui.main.data.BaseData
 import com.school.soundeditor.ui.main.data.MovieData
 import com.school.soundeditor.ui.main.data.TrackData
 import kotlinx.android.synthetic.main.fragment_playback.*
 import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import java.io.RandomAccessFile
+import java.nio.channels.FileChannel
+import java.util.*
 
 
 internal class PlaybackFragment : Fragment(), PlaybackScreenView {
@@ -28,6 +48,16 @@ internal class PlaybackFragment : Fragment(), PlaybackScreenView {
     private val myHandler: Handler = Handler()
     private var runnableTimeCounter = 0
     private var isAudioFilePlaying = false
+
+    private val TRIMMED_FILE = "outputAfterTrim.mp4"
+    private val LOOPED_FILE = "loopedAfterTrim.mp4"
+
+    private var fileSrc: String? = null
+    private val PERMISSIONS_STORAGE = arrayOf(
+        Manifest.permission.READ_EXTERNAL_STORAGE,
+        Manifest.permission.WRITE_EXTERNAL_STORAGE
+    )
+    private val REQUEST_EXTERNAL_STORAGE = 1
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -73,6 +103,7 @@ internal class PlaybackFragment : Fragment(), PlaybackScreenView {
                 trackDetailLayout.findViewById<TextView>(R.id.track_format_text_view).text =
                     data.format
                 data.fileSrc?.let { handleFileChosenMediaPlayer(it) }
+                fileSrc = data.fileSrc
                 container.addView(trackDetailLayout)
             }
             is MovieData -> {
@@ -95,6 +126,7 @@ internal class PlaybackFragment : Fragment(), PlaybackScreenView {
             else -> {
             }
         }
+
         seekPlayAudio.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
                 showMinutesSeconds(seekPlayAudio.progress)
@@ -108,6 +140,40 @@ internal class PlaybackFragment : Fragment(), PlaybackScreenView {
                 runnableTimeCounter = seekPlayAudio.progress
             }
         })
+        btnTrimAndLoop.setOnClickListener {
+            handleTrimAndLoopButtonClick()
+        }
+        btnAdd.setOnClickListener {
+            addToLoop()
+        }
+        btnSubtract.setOnClickListener {
+            subtractFromLoop()
+        }
+        seekBarStartTime.setOnSeekBarChangeListener(object : OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar, i: Int, b: Boolean) {
+                //convert i which is in value of milliseconds to seconds with one decimal
+                val value = i / 100
+                //set value of text view of seek bar
+                tvSeekStart.setText((value / 10.0).toString() + "s")
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar) {}
+            override fun onStopTrackingTouch(seekBar: SeekBar) {}
+        })
+        seekBarEndTime.setOnSeekBarChangeListener(object : OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar, i: Int, b: Boolean) {
+                //convert i which is in value of milliseconds to seconds with one decimal
+                val value = i / 100
+                tvSeekEnd.setText((value / 10.0).toString() + "s")
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar) {}
+            override fun onStopTrackingTouch(seekBar: SeekBar) {}
+        })
+        //get duration of audio file
+        val fileDuration = getAudioFileDuration(fileSrc!!)
+        //set seek bar limit to 90% of file duration
+        configureSeekBars(fileDuration - (fileDuration * 0.1).toInt())
     }
 
     private fun showMinutesSeconds(positionToShow: Int) {
@@ -190,6 +256,294 @@ internal class PlaybackFragment : Fragment(), PlaybackScreenView {
 
     override fun showTrack(mp3: String) {
         Toast.makeText(requireContext(), mp3, Toast.LENGTH_SHORT).show()
+    }
+
+    /**
+     * Subtracts one from the value of the loop field.
+     */
+    private fun subtractFromLoop() {
+        var tvLoopNumber = Integer.valueOf(tvLoop!!.text.toString())
+        if (tvLoopNumber == 1) return
+        tvLoopNumber--
+        tvLoop.setText(tvLoopNumber.toString() + "")
+    }
+
+    /**
+     * Adds one to the value of the loop field.
+     */
+    private fun addToLoop() {
+        var tvLoopNumber = Integer.valueOf(tvLoop!!.text.toString())
+        tvLoopNumber++
+        tvLoop.setText(tvLoopNumber.toString() + "")
+    }
+
+    /**
+     * Handles trim and loop button click.
+     */
+    private fun handleTrimAndLoopButtonClick() {
+        if (fileSrc == null) {
+            //if fileSource null tell the user to record an audio or choose a file
+            Toast.makeText(requireContext(), "Please record an audio or choose a file", Toast.LENGTH_SHORT)
+                .show()
+            return
+        }
+        val tvLoopString = tvLoop!!.text.toString()
+        val loopNumber = Integer.valueOf(tvLoopString)
+        if (loopNumber == 0) {
+            Toast.makeText(requireContext(), "Please Enter Number Greater than 0", Toast.LENGTH_SHORT).show()
+            return
+        }
+        val fileDuration = getAudioFileDuration(fileSrc!!)
+        var trimFromStartTime = 0.0
+        var trimFromEndTime = 0.0
+        trimFromStartTime = seekBarStartTime!!.progress.toDouble()
+        trimFromEndTime = seekBarEndTime!!.progress.toDouble()
+        //trim file from start time to end time
+        val endTime = fileDuration - trimFromEndTime
+        trimFile(fileSrc!!, trimFromStartTime / 1000.0, endTime / 1000.0)
+        //loop file
+        loopFile(TRIMMED_FILE, loopNumber)
+    }
+
+    /**
+     * Trims a file to get a result of the file from startTime to endTime.
+     */
+    private fun trimFile(inputFilePath: String, startTime: Double, endTime: Double) {
+        try {
+            // get file from memory
+            val dir = File(Environment.getExternalStorageDirectory(), "/mp4Test/")
+            dir.mkdirs()
+            val inputFile = File(inputFilePath)
+            // create movie from the file
+            val movie = MovieCreator.build(inputFile.path)
+
+            // get tracks from movie
+            val tracks = movie.tracks
+            movie.tracks = LinkedList()
+
+            //start time which will be updated according to samples
+            var startTimeAfterSync = startTime
+            //end time which will be updated according to samples
+            var endTimeAfterSync = endTime
+            var timeCorrected = false
+
+            // Here we try to find a track that has sync samples. Since we can only start decoding
+            // at such a sample we SHOULD make sure that the start of the new fragment is exactly
+            // such a frame
+            for (track in tracks) {
+                if (track.syncSamples != null && track.syncSamples.size > 0) {
+                    if (timeCorrected) {
+                        // This exception here could be a false positive in case we have multiple tracks
+                        // with sync samples at exactly the same positions. E.g. a single movie containing
+                        // multiple qualities of the same video (Microsoft Smooth Streaming file)
+                        throw RuntimeException("The startTime has already been corrected by another track with SyncSample. Not Supported.")
+                    }
+                    startTimeAfterSync = correctTimeToSyncSample(track, startTimeAfterSync, false)
+                    endTimeAfterSync = correctTimeToSyncSample(track, endTimeAfterSync, true)
+                    timeCorrected = true
+                }
+            }
+            for (track in tracks) {
+                var currentSample: Long = 0
+                var currentTime = 0.0
+                var lastTime = -1.0
+                var startSample1: Long = -1
+                var endSample1: Long = -1
+                for (i in track.sampleDurations.indices) {
+                    val delta = track.sampleDurations[i]
+                    if (currentTime > lastTime && currentTime <= startTimeAfterSync) {
+                        // current sample is still before the new starttime
+                        startSample1 = currentSample
+                    }
+                    if (currentTime > lastTime && currentTime <= endTimeAfterSync) {
+                        // current sample is after the new start time and still before the new endtime
+                        endSample1 = currentSample
+                    }
+                    lastTime = currentTime
+                    currentTime += delta.toDouble() / track.trackMetaData.timescale.toDouble()
+                    currentSample++
+                }
+                movie.addTrack(AppendTrack(CroppedTrack(track, startSample1, endSample1)))
+            }
+            val start1 = System.currentTimeMillis()
+            val out = DefaultMp4Builder().build(movie)
+            val start2 = System.currentTimeMillis()
+            val output = File("$dir/$TRIMMED_FILE")
+            if (output.exists()) {
+                output.delete()
+            }
+            val fos =
+                FileOutputStream("$dir/$TRIMMED_FILE") //String.format("output-%f-%f.mp4", startTime1, endTime1)
+            val fc: FileChannel = fos.getChannel()
+            out.writeContainer(fc)
+            fc.close()
+            fos.close()
+            val start3 = System.currentTimeMillis()
+            System.err.println("Building IsoFile took : " + (start2 - start1) + "ms")
+            System.err.println("Writing IsoFile took  : " + (start3 - start2) + "ms")
+        } catch (e: java.lang.Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    /**
+     * Syncs time with nearest sample, since trim can only be done from the start of a sample
+     */
+    private fun correctTimeToSyncSample(track: Track, cutHere: Double, next: Boolean): Double {
+        val timeOfSyncSamples = DoubleArray(track.syncSamples.size)
+        var currentSample: Long = 0
+        var currentTime = 0.0
+        for (i in track.sampleDurations.indices) {
+            val delta = track.sampleDurations[i]
+            if (Arrays.binarySearch(track.syncSamples, currentSample + 1) >= 0) {
+                // samples always start with 1 but we start with zero therefore +1
+                timeOfSyncSamples[Arrays.binarySearch(track.syncSamples, currentSample + 1)] =
+                    currentTime
+            }
+            currentTime += delta.toDouble() / track.trackMetaData.timescale.toDouble()
+            currentSample++
+        }
+        var previous = 0.0
+        for (timeOfSyncSample in timeOfSyncSamples) {
+            if (timeOfSyncSample > cutHere) {
+                return if (next) {
+                    timeOfSyncSample
+                } else {
+                    previous
+                }
+            }
+            previous = timeOfSyncSample
+        }
+        return timeOfSyncSamples[timeOfSyncSamples.size - 1]
+    }
+
+    /**
+     * Creates a loop of the given audio file according to the given loop number.
+     */
+    private fun loopFile(inputFilePath: String, loopNumber: Int) {
+        verifyStoragePermissions(requireActivity())
+        val dir = File(Environment.getExternalStorageDirectory(), "/mp4Test/")
+        dir.mkdirs()
+        //file which we will work on
+        val inputFile = File(dir, inputFilePath)
+        //file to be saved
+        val outputFile = File(dir, LOOPED_FILE)
+        // create movie from input file
+        var inputMovie: Movie? = null
+        try {
+            inputMovie = MovieCreator.build(inputFile.absolutePath)
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+        // create array of input movies
+        val inputMovies = arrayOfNulls<Movie>(loopNumber)
+        for (i in 0 until loopNumber) {
+            inputMovies[i] = inputMovie
+        }
+        // create output movie
+        val outputMovie = Movie()
+        // create audio tracks
+        val audioTracks: MutableList<Track> = ArrayList()
+        for (movie in inputMovies) {
+            if (movie != null) {
+                for (track in movie.getTracks()) {
+                    if (track.getHandler().equals("soun")) {
+                        audioTracks.add(track)
+                    }
+                }
+            }
+        }
+        // add audio tracks to output movie
+        try {
+            outputMovie.addTrack(AppendTrack(*audioTracks.toTypedArray()))
+        } catch (e: java.lang.Exception) {
+            e.printStackTrace()
+        }
+        // save output to file
+        var container: Container? = null
+        try {
+            container = DefaultMp4Builder().build(outputMovie)
+        } catch (e: java.lang.Exception) {
+        }
+        var fc: FileChannel? = null
+        try {
+            fc = RandomAccessFile(outputFile, "rw").getChannel()
+        } catch (e: java.lang.Exception) {
+            e.printStackTrace()
+        }
+        try {
+            if (container != null) {
+                container.writeContainer(fc)
+            }
+            Toast.makeText(requireContext(), "File was saved", Toast.LENGTH_SHORT).show()
+            tvSavedFile!!.text = "File Saved: " + outputFile.path
+            handleLoopedFileSaved(outputFile.path)
+        } catch (e: java.lang.Exception) {
+            Toast.makeText(requireContext(), "File failed to be saved", Toast.LENGTH_SHORT).show()
+        }
+        try {
+            if (fc != null) {
+                fc.close()
+            }
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+    }
+
+    /**
+     * Handles the evenet of saving a new audio file
+     * @param fileSrc is the path of the saved file
+     */
+    private fun handleLoopedFileSaved(fileSrc: String) {
+        handleFileChosenMediaPlayer(fileSrc)
+    }
+
+    /**
+     * Configure Seek bars according to passed audio duration
+     * @param fileDuration is the duration of the file
+     */
+    private fun configureSeekBars(fileDuration: Int) {
+        seekBarStartTime!!.max = fileDuration / 2
+        seekBarEndTime!!.max = fileDuration / 2
+    }
+
+
+    /**
+     * Returns the duration of the audio file found at the given path.
+     * If anything went wrong, 0 will be returned.
+     */
+    private fun getAudioFileDuration(filePath: String): Int {
+        var duration = 0
+        try {
+            val mmr = MediaMetadataRetriever()
+            mmr.setDataSource(filePath)
+            val strDuration = mmr.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION)
+            mmr.release()
+            duration = strDuration!!.toInt()
+        } catch (e: Exception) {
+            // nothing to be done
+        }
+        return duration
+    }
+
+    /**
+     * Checks if the app has permission to write to device storage
+     * If the app does not has permission then the user will be prompted to grant permissions
+     */
+    fun verifyStoragePermissions(activity: Activity?) {
+        // Check if we have write permission
+        val permission = ActivityCompat.checkSelfPermission(
+            activity!!,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+        )
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            // We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(
+                activity,
+                PERMISSIONS_STORAGE,
+                REQUEST_EXTERNAL_STORAGE
+            )
+        }
     }
 
     companion object {
